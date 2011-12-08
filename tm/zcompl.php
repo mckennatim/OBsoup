49e66d1368805958c264fb9959eec78e4dfc8205
Timothy McKenna <mckenna.tim@gmail.com>
2011-12-08 11:17:46 -0500

    branch test

--- tm/zcompl.php
+++ tm/zcompl.php
@@ -6,13 +6,13 @@ require_once('fb.php');
 ob_start(); //gotta have this
 fb('in zcompl');
 
-$zip=$_GET[zip];
+$zip=$_GET['zip'];
 
 mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) or die("can't even connect");
-mysql_select_db (DB_DATABASE) or die("db unavailable");	
+mysql_select_db (DB_DATABASE) or die("db unavailable");
 
 
-$trying ="LOOKING UP Z IPCODE ".$pid." is complete"; fb($trying);
+$trying ="LOOKING UP Z IPCODE"; fb($trying);
 $sql = "SELECT *
 FROM zip_codes
 WHERE zip = ".$zip ;
@@ -24,11 +24,11 @@ $row = mysql_fetch_assoc($result);
 $ret['zip'] = $row['zip'];
 $ret['value'] = $row['zip'];
 $ret['city'] = $row['city'];
-$ret['state'] = $row['state'];	
+$ret['state'] = $row['state'];
 
 
 /* Free connection resources. */
-mysql_close($conn);
+mysql_close();
 fb(json_encode($ret));
 echo json_encode($ret);
 ?>
